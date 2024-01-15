2d5bd0c5b345a4f6320073ec9641d2da96ccf92b
IngridGdesigns <garciai@protonmail.com>
2024-01-15 12:06:29 -0800

    rename id and added toggle logic - WIP

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const btnClipboard = document.getElementById("btn-copy");
 const inputLength = document.getElementById("input-length");
 
 
-function getRandom(passwordLength = 15){
-  let pass = [];
+
+function getRandom(passwordLength = 15, isToggleChecked){
+    let pass = [];
 
     for(let i = 0; i < passwordLength; i++){
         let randomNum = Math.floor(Math.random() * characters.length) + 1;
@@ -26,8 +27,32 @@ btnEl.addEventListener("click", () => {
         passwordLength = 15;
     }
     
+    const isChecked = getValue();
+    const arry = isInputChecked(isChecked);
+    
     firstPwd.textContent = getRandom(passwordLength);
     secondPwd.textContent = getRandom(passwordLength);
 });
 
+function isInputChecked(isChecked) {
+    if(!isChecked){
+        console.log('not checked')
+        return ['a', 'b']
+    } 
+    console.log('checked');
+    return characters
+    
+}
+
+function getValue() {
+    const isChecked = document.getElementById("input-symbol").checked;
+
+   if(isChecked){
+     console.log("Input is checked");
+     return true;
+   } else {
+     console.log("Input is NOT checked");
+     return false;
+   }
+}
 
