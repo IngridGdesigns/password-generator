facbd93833d9792acb0e368a5a44d9aa7a60b0d9
IngridGdesigns <garciai@protonmail.com>
2024-01-15 12:27:41 -0800

    added regex to filter out array, removed logs

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a",
+    "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4",
+    "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*",
+    "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";",
+    "<", ">", ".", "?", "/"];
 
 const btnEl = document.getElementById("btn-el");
 const firstPwd = document.getElementById("first-pwd");
@@ -8,13 +13,12 @@ const btnClipboard = document.getElementById("btn-copy");
 const inputLength = document.getElementById("input-length");
 
 
-
-function getRandom(passwordLength = 15, isToggleChecked){
+function getRandom(passwordLength = 15, isCheckedArray){
     let pass = [];
 
     for(let i = 0; i < passwordLength; i++){
-        let randomNum = Math.floor(Math.random() * characters.length) + 1;
-        let randomChar = characters[randomNum];
+        let randomNum = Math.floor(Math.random() * isCheckedArray.length) + 1;
+        let randomChar = isCheckedArray[randomNum];
         pass.push(randomChar);
     }
     return pass.join('');
@@ -28,18 +32,21 @@ btnEl.addEventListener("click", () => {
     }
     
     const isChecked = getValue();
-    const arry = isInputChecked(isChecked);
+    const isCheckedArray = isInputChecked(isChecked);
     
-    firstPwd.textContent = getRandom(passwordLength);
-    secondPwd.textContent = getRandom(passwordLength);
+    firstPwd.textContent = getRandom(passwordLength, isCheckedArray);
+    secondPwd.textContent = getRandom(passwordLength, isCheckedArray);
 });
 
 function isInputChecked(isChecked) {
+
     if(!isChecked){
-        console.log('not checked')
-        return ['a', 'b']
+        const regex = /[a-zA-Z0-9]/g;
+        const filtered = characters.filter((character => character.match(regex)));
+        
+        return filtered
     } 
-    console.log('checked');
+  
     return characters
     
 }
@@ -48,10 +55,8 @@ function getValue() {
     const isChecked = document.getElementById("input-symbol").checked;
 
    if(isChecked){
-     console.log("Input is checked");
      return true;
    } else {
-     console.log("Input is NOT checked");
      return false;
    }
 }
